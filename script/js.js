877c44fd26797427491ed2a72fec5f2805c5a169
xiaowei <240942649@qq.com>
2019-08-13 00:58:42 +0800

    js文件

---
+++ script/js.js
@@ -0,0 +1,510 @@
+
+var _$lz = undefined;
+var _$kx = String.fromCharCode;
+var _$lq;
+var _$lp = Array;
+var _$lf = Math;
+var _$cU = 'charCodeAt';
+var _$et = Math.random;
+var _$aQ = Date;
+var _$lr = 25165824;
+var _$lm = 1;
+var _$kK;
+var _$cD = 'qrcklmDoExthWJiHAp1sVYKU3RFMQw8IGfPO92bvLNj.7zXBaSnu0TC6gy_4Ze5d'['split']('');
+var _$cg;
+
+_$fH();
+function _$fH() {
+    var _$iC = [[], [], [], [], []];
+    var _$dh = [[], [], [], [], []];
+    _$cg = function(_$c1) {
+        return [_$iC, _$dh];
+    }
+    ;
+}
+
+var _$iC = [];
+
+for (var _$ba = 0; _$ba < 6; _$ba++) {
+    _$iC[_$ba] = [];
+}
+
+_$kq = function() {
+    var dd = [[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,272,276,null,280,284,288,null,null,292,296,300,304,308,312,172,null,208,72,148,96,236,248,220,176,120,144,316,null,null,320,null,324,328,64,188,216,24,32,104,128,60,124,52,88,160,108,164,140,136,112,100,196,212,92,80,48,184,84,240,332,null,336,340,232,null,192,152,8,252,244,132,224,44,56,168,12,16,20,200,28,68,0,4,76,40,204,156,116,36,228,180,256,264,260,268],[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,4,4,null,4,4,4,null,null,4,4,4,4,4,4,2,null,3,1,2,1,3,3,3,2,1,2,4,null,null,5,null,5,5,1,2,3,0,0,1,2,0,1,0,1,2,1,2,2,2,1,1,3,3,1,1,0,2,1,3,5,null,5,5,3,null,3,2,0,3,3,2,3,0,0,2,0,0,0,3,0,1,0,0,1,0,3,2,1,0,3,2,4,4,4,4],[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,64,80,null,96,112,128,null,null,144,160,176,192,208,224,176,null,64,32,80,128,176,224,112,192,224,64,240,null,null,0,null,16,32,0,240,96,96,128,160,0,240,240,208,96,128,176,144,48,32,192,144,16,80,112,64,192,224,80,192,48,null,64,80,160,null,0,96,32,240,208,16,128,176,224,160,48,64,80,32,112,16,0,16,48,160,48,112,208,144,144,208,0,32,16,48],[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,17,17,null,17,17,18,null,null,18,18,18,19,19,19,10,null,13,4,9,6,14,15,13,11,7,9,19,null,null,20,null,20,20,4,11,13,1,2,6,8,3,7,3,5,10,6,10,8,8,7,6,12,13,5,5,3,11,5,15,20,null,21,21,14,null,12,9,0,15,15,8,14,2,3,10,0,1,1,12,1,4,0,0,4,2,12,9,7,2,14,11,16,16,16,16],[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0,64,null,128,192,0,null,null,64,128,192,0,64,128,192,null,0,128,64,0,192,128,192,0,128,0,192,null,null,0,null,64,128,0,192,128,128,0,128,0,192,192,64,128,0,192,64,192,128,0,64,64,64,192,0,0,128,64,0,192,null,0,64,128,null,0,128,128,192,64,64,0,192,128,128,192,0,64,128,192,64,0,64,192,128,192,192,64,64,64,64,0,128,64,192],[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,68,69,-1,70,71,72,-1,-1,73,74,75,76,77,78,43,-1,52,18,37,24,59,62,55,44,30,36,79,-1,-1,80,-1,81,82,16,47,54,6,8,26,32,15,31,13,22,40,27,41,35,34,28,25,49,53,23,20,12,46,21,60,83,-1,84,85,58,-1,48,38,2,63,61,33,56,11,14,42,3,4,5,50,7,17,0,1,19,10,51,39,29,9,57,45,64,66,65,67,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]]
+    return dd;
+};
+
+
+function get_cookie(go) {
+    
+    _$lq = _$jp(_$jD(go));
+    var _$dh = 'K';
+    var _$iC = _$ai();
+    var _$gi = _$h1();
+    var _$c1 = _$aU();
+    var _$hp = _$dh + _$gI(_$gi['concat'](_$eO(_$c1, _$iC)));
+    return _$hp
+}
+
+function _$jD(go) {
+    return _$kS(go);
+}
+
+function _$kS(_$c1) {
+    var _$dh, _$gi = _$c1.length, _$kM = [], _$iC = _$c1.charCodeAt(0) - 93;
+    for (var _$hp = 0, _$a7 = 1; _$a7 < _$gi; ++_$a7) {
+        _$dh = _$c1.charCodeAt(_$a7);
+        if (_$dh >= 40 && _$dh < 92) {
+            _$dh += _$iC;
+            if (_$dh >= 92)
+                _$dh = _$dh - 52;
+        } else if (_$dh >= 93 && _$dh < 127) {
+            _$dh += _$iC;
+            if (_$dh >= 127)
+                _$dh = _$dh - 34;
+        }
+        _$kM[_$hp++] = _$dh;
+    }
+    return _$kx.apply(null, _$kM);
+}
+function _$ai() {
+    return _$ky(_$lq['cookieKey'])['slice'](0, 32);
+}
+
+function _$ky(_$a7) {
+    var _$ip = _$a7.length
+      , _$cx = new _$lp(_$lf['floor'](_$ip * 3 / 4))
+      , _$cl = _$cU;
+    var _$aT, _$dR, _$je, _$ji;
+    var _$kM = 0
+      , _$ba = 0
+      , _$c1 = _$ip - 3;
+    var _$iC = _$kq();
+    var _$i9 = _$iC[0]
+      , _$jm = _$iC[1]
+      , _$gi = _$iC[2]
+      , _$hp = _$iC[3]
+      , _$gz = _$iC[4]
+      , _$dh = _$iC[5];
+    for (_$kM = 0; _$kM < _$c1; ) {
+        _$aT = _$a7[_$cl](_$kM++);
+        _$dR = _$a7[_$cl](_$kM++);
+        _$je = _$a7[_$cl](_$kM++);
+        _$ji = _$a7[_$cl](_$kM++);
+        _$cx[_$ba++] = _$i9[_$aT] | _$jm[_$dR];
+        _$cx[_$ba++] = _$gi[_$dR] | _$hp[_$je];
+        _$cx[_$ba++] = _$gz[_$je] | _$dh[_$ji];
+    }
+    if (_$kM < _$ip) {
+        _$aT = _$a7[_$cl](_$kM++);
+        _$dR = _$a7[_$cl](_$kM++);
+        _$cx[_$ba++] = _$i9[_$aT] | _$jm[_$dR];
+        if (_$kM < _$ip) {
+            _$je = _$a7[_$cl](_$kM);
+            _$cx[_$ba++] = _$gi[_$dR] | _$hp[_$je];
+        }
+    }
+    return _$cx;
+}
+
+function _$jp(_$gz) {
+    
+    var _$kM = {};
+    
+    var _$ba = _$gz['substr'](0, _$gz.length - 2);
+    
+    var _$hp = parseInt(_$gz['substr'](_$gz.length - 2), 16);
+
+    
+    var _$dh = _$ba['split']('`');
+    
+    for (var _$a7 = 0; _$a7 < _$dh.length; _$a7++) {
+        var _$iC = _$dh[_$a7]['indexOf'](':');
+        if (_$iC > 0) {
+            var _$ip = _$dh[_$a7]['slice'](0, _$iC);
+            var _$gi = _$dh[_$a7]['slice'](_$iC + 1);
+            _$kM[_$ip] = _$gi;
+        }
+    }
+    return _$kM;
+
+
+}
+
+function _$h1() {
+    return _$ky(_$lq['blackBlock'])['slice'](0, 16);
+}
+
+function _$aU(_$dh) {
+    var _$c1 = _$k7(0xffffffff);
+    
+    var _$hp = (new _$aQ()['getTime']()) % 0xffffffff;
+    
+    
+    var _$iC = _$kT();
+    var _$a7 = _$kA([_$c1, _$hp]);
+    _$a7 = _$a7['concat'](_$iC);
+    
+    _$a7.push(_$dc(_$a7));
+    return _$a7;
+}
+function _$k7(_$dh) {
+    return _$lf['floor'](_$et() * _$dh);
+}
+function _$kT() {
+    
+    var _$dh = [1, _$kA([_$lr]), _$lm, _$bJ()];
+    return _$lp['prototype']['concat']['apply']([], _$dh);
+}
+
+function _$ku() {
+    
+    if (_$k8 <= 8) {
+        return;
+    }
+    var _$ba = 0
+      , _$hp = '_Selenium_IDE_Recorder,_selenium,callSelenium'
+      , _$kM = '__driver_evaluate,__webdriver_evaluate,__selenium_evaluate,__fxdriver_evaluate,__driver_unwrapped,__webdriver_unwrapped,__selenium_unwrapped,__fxdriver_unwrapped,__webdriver_script_func,__webdriver_script_fn'
+      , _$dh = ['selenium', 'webdriver', 'driver'];
+    try {
+        var _$iC = false;
+        if (_$lm === 2) {
+            var _$ip = _$lE['navigator']['userAgent']['toLowerCase']()['match'](/firefox\/(\d+\.\d+)/)[1];
+            _$iC = _$jX(_$ip) >= 60;
+        }
+        _$ba = _$ly(_$lE, _$hp) || _$ly(_$lD, _$kM) || (_$lE['clientInformation'] && _$lE['clientInformation']['webdriver']) || (_$iC && _$lE['navigator']['webdriver']);
+        for (var _$gi in _$lD) {
+            if (_$gi[0] === '$' && _$gi['match']('^\\$[a-z]dc_') && _$lD[_$gi]['cache_']) {
+                _$ba = 1;
+            }
+        }
+        for (var _$a7 = 0; _$a7 < _$dh.length; _$a7++) {
+            if (_$lD['documentElement']['getAttribute'](_$dh[_$a7]))
+                _$ba = 1;
+        }
+    } catch (_$c1) {}
+    
+    if (_$ba) {
+        _$lE._$j3 = 1;
+        _$lx(134217728, 31);
+    }
+}
+
+function _$kA(_$dh) {
+    var _$gi = _$dh.length, _$iC = 0, _$a7 = 0, _$hp = _$dh.length * 4, _$c1, _$kM;
+    _$kM = new _$lp(_$hp);
+    while (_$iC < _$gi) {
+        _$c1 = _$dh[_$iC++];
+        _$kM[_$a7++] = (_$c1 >>> 24) & 0xFF;
+        _$kM[_$a7++] = (_$c1 >>> 16) & 0xFF;
+        _$kM[_$a7++] = (_$c1 >>> 8) & 0xFF;
+        _$kM[_$a7++] = _$c1 & 0xFF;
+    }
+    return _$kM;
+}
+function _$bJ() {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    _$kK = [238,219,47,182,148,149,95,170,161,22,119,6,125,32,193,20,254,24,233,81]
+    return _$kK;
+}
+function _$dc(_$dh) {
+
+    var _$gi = _$iZ();
+    var _$hp = 0
+      , _$a7 = _$dh.length
+      , _$c1 = 0;
+    while (_$c1 < _$a7) {
+        _$hp = _$gi[(_$hp ^ _$dh[_$c1++]) & 0xFF];
+    }
+    return _$hp;
+}
+function _$js(_$dh) {
+    return _$dh['toString']()['match'](/{\s*return\s*([A-Za-z0-9$_]+);?\s*}/)[1];
+}
+
+function _$gI(_$c1) {
+    var _$iC = _$k7(255);
+    var _$a7 = [];
+    _$a7.push(_$iC);
+    for (var _$dh = 0; _$dh < _$c1.length; _$dh++) {
+        _$a7.push(_$iC ^ _$c1[_$dh]);
+    }
+    return _$i2(_$a7);
+}
+function _$eO(_$iC, _$a7, _$c1) {
+    if (typeof _$iC === 'string')
+        _$iC = _$k4(_$iC);
+    var _$dh = _$kd(_$a7, _$c1);
+    var dd = _$dh._$jO(_$iC, true)
+    return dd;
+}
+
+function _$kd(_$ba, _$c1) {
+    var _$dh = _$cg()
+      , _$hp = _$dh[0]
+      , _$ip = _$dh[1];
+
+
+    if (!_$hp[0][0] && !_$hp[0][1]) {
+        _$fA(_$c1, _$hp, _$ip);
+    }
+    var _$gi = _$jH(_$ba, _$hp, _$ip);
+    ;;var _$iC = {};
+    _$iC._$jO = _$a7;
+    _$iC._$iO = _$kM;
+    return _$iC;
+    function _$a7(_$i9, _$cx) {
+        var _$jm = _$lf['floor'](_$i9.length / 16) + 1, _$ji, _$je = [], _$dR = 16 - (_$i9.length % 16), _$cl, _$gz;
+        if (_$cx) {
+            _$je = _$cl = _$cS();
+        }
+        var _$aT = _$i9['slice'](0);
+        _$gz = _$i9.length + _$dR;
+        for (_$ji = _$i9.length; _$ji < _$gz; )
+            _$aT[_$ji++] = _$dR;
+        _$aT = _$ls(_$aT);
+        for (_$ji = 0; _$ji < _$jm; ) {
+            _$gz = _$aT['slice'](_$ji << 2, (++_$ji) << 2);
+            _$gz = _$cl ? _$kI(_$gz, _$cl) : _$gz;
+            _$cl = _$jj(_$gi, _$gz, 0, _$hp);
+            _$je = _$je['concat'](_$cl);
+        }
+        return _$kA(_$je);
+    }
+    function _$kM(_$cx, _$aT) {
+        var _$jm, _$cl, _$i9, _$gz, _$ji = [], _$je, _$dR;
+        _$cx = _$ls(_$cx);
+        if (_$aT) {
+            _$dR = _$cx['slice'](0, 4);
+            _$cx = _$cx['slice'](4);
+        }
+        _$jm = _$cx.length / 4;
+        for (_$cl = 0; _$cl < _$jm; ) {
+            _$gz = _$cx['slice'](_$cl << 2, (++_$cl) << 2);
+            _$i9 = _$jj(_$gi, _$gz, 1, _$ip);
+            _$ji = _$ji['concat'](_$dR ? _$kI(_$i9, _$dR) : _$i9);
+            _$dR = _$gz;
+        }
+        _$ji = _$kA(_$ji);
+        _$je = _$ji[_$ji.length - 1];
+        _$ji['splice'](_$ji.length - _$je, _$je);
+        return _$ji;
+    }
+}
+
+function _$fA(_$c1, _$ip, _$dR) {
+    var _$a7 = _$ip[4], _$ji = _$dR[4], _$hp, _$je, _$gz, _$cx = [], _$cl = [], _$kM, _$aT, _$ba, _$gi, _$iC, _$dh;
+    for (_$hp = 0; _$hp < 256; _$hp++) {
+        _$cl[(_$cx[_$hp] = _$hp << 1 ^ (_$hp >> 7) * 283) ^ _$hp] = _$hp;
+    }
+    for (_$je = _$gz = 0; !_$a7[_$je]; _$je ^= _$kM || 1,
+    _$gz = _$cl[_$gz] || 1) {
+        _$gi = _$gz ^ _$gz << 1 ^ _$gz << 2 ^ _$gz << 3 ^ _$gz << 4;
+        _$gi = _$gi >> 8 ^ _$gi & 255 ^ 99;
+        _$a7[_$je] = _$gi;
+        _$ji[_$gi] = _$je;
+        _$kM = _$cx[_$je];
+    }
+    for (_$hp = 0; _$hp < 256; _$hp++) {
+        _$ji[_$a7[_$hp]] = _$hp;
+    }
+    for (_$je = 0; _$je < 256; _$je++) {
+        _$gi = _$a7[_$je];
+        _$ba = _$cx[_$aT = _$cx[_$kM = _$cx[_$je]]];
+        _$dh = _$ba * 0x1010101 ^ _$aT * 0x10001 ^ _$kM * 0x101 ^ _$je * 0x1010100;
+        _$iC = _$cx[_$gi] * 0x101 ^ _$gi * 0x1010100;
+        for (_$hp = 0; _$hp < 4; _$hp++) {
+            _$ip[_$hp][_$je] = _$iC = _$iC << 24 ^ _$iC >>> 8;
+            _$dR[_$hp][_$gi] = _$dh = _$dh << 24 ^ _$dh >>> 8;
+        }
+    }
+    for (_$hp = 0; _$hp < 5; _$hp++) {
+        _$ip[_$hp] = _$ip[_$hp]['slice'](0);
+        _$dR[_$hp] = _$dR[_$hp]['slice'](0);
+    }
+}
+
+function _$jH(_$kM, _$ip, _$je) {
+    var _$iC = _$kM;
+    if (_$kM.length % 16 !== 0)
+        _$iC = _$h9(_$kM);
+    var _$gz = _$ls(_$iC);
+    var _$a7, _$gi, _$dh, _$ba, _$ji, _$c1 = _$ip[4], _$hp = _$gz.length, _$cl = 1;
+    var _$ba = _$gz['slice'](0);
+    var _$ji = [];
+    for (_$a7 = _$hp; _$a7 < 4 * _$hp + 28; _$a7++) {
+        _$dh = _$ba[_$a7 - 1];
+        if (_$a7 % _$hp === 0 || (_$hp === 8 && _$a7 % _$hp === 4)) {
+            _$dh = _$c1[_$dh >>> 24] << 24 ^ _$c1[_$dh >> 16 & 255] << 16 ^ _$c1[_$dh >> 8 & 255] << 8 ^ _$c1[_$dh & 255];
+            if (_$a7 % _$hp === 0) {
+                _$dh = _$dh << 8 ^ _$dh >>> 24 ^ _$cl << 24;
+                _$cl = _$cl << 1 ^ (_$cl >> 7) * 283;
+            }
+        }
+        _$ba[_$a7] = _$ba[_$a7 - _$hp] ^ _$dh;
+    }
+    for (_$gi = 0; _$a7; _$gi++,
+    _$a7--) {
+        _$dh = _$ba[_$gi & 3 ? _$a7 : _$a7 - 4];
+        if (_$a7 <= 4 || _$gi < 4) {
+            _$ji[_$gi] = _$dh;
+        } else {
+            _$ji[_$gi] = _$je[0][_$c1[_$dh >>> 24]] ^ _$je[1][_$c1[_$dh >> 16 & 255]] ^ _$je[2][_$c1[_$dh >> 8 & 255]] ^ _$je[3][_$c1[_$dh & 255]];
+        }
+    }
+    return [_$ba, _$ji];
+}
+
+function _$ls(_$iC) {
+    var _$hp = _$iC.length / 4
+      , _$c1 = 0
+      , _$a7 = 0
+      , _$gi = _$iC.length;
+    var _$dh = new _$lp(_$hp);
+    while (_$c1 < _$gi) {
+        _$dh[_$a7++] = ((_$iC[_$c1++] << 24) | (_$iC[_$c1++] << 16) | (_$iC[_$c1++] << 8) | (_$iC[_$c1++]));
+    }
+    return _$dh;
+}
+
+function _$cS() {
+    return [_$k7(0xFFFFFFFF), _$k7(0xFFFFFFFF), _$k7(0xFFFFFFFF), _$k7(0xFFFFFFFF)];
+}
+
+function _$kI(_$iC, _$dh) {
+    return [(_$iC[0] ^ _$dh[0]), (_$iC[1] ^ _$dh[1]), (_$iC[2] ^ _$dh[2]), (_$iC[3] ^ _$dh[3])];
+}
+
+function _$jj(_$kM, _$i9, _$df, _$jm) {
+    var _$cx = _$kM[_$df], _$dh = _$i9[0] ^ _$cx[0], _$c1 = _$i9[_$df ? 3 : 1] ^ _$cx[1], _$iC = _$i9[2] ^ _$cx[2], _$a7 = _$i9[_$df ? 1 : 3] ^ _$cx[3], _$dR, _$gz, _$ji, _$aT = _$cx.length / 4 - 2, _$je, _$ij = 4, _$iR = [0, 0, 0, 0], _$ip = _$jm[0], _$cl = _$jm[1], _$ba = _$jm[2], _$gi = _$jm[3], _$hp = _$jm[4];
+    for (_$je = 0; _$je < _$aT; _$je++) {
+        _$dR = _$ip[_$dh >>> 24] ^ _$cl[_$c1 >> 16 & 255] ^ _$ba[_$iC >> 8 & 255] ^ _$gi[_$a7 & 255] ^ _$cx[_$ij];
+        _$gz = _$ip[_$c1 >>> 24] ^ _$cl[_$iC >> 16 & 255] ^ _$ba[_$a7 >> 8 & 255] ^ _$gi[_$dh & 255] ^ _$cx[_$ij + 1];
+        _$ji = _$ip[_$iC >>> 24] ^ _$cl[_$a7 >> 16 & 255] ^ _$ba[_$dh >> 8 & 255] ^ _$gi[_$c1 & 255] ^ _$cx[_$ij + 2];
+        _$a7 = _$ip[_$a7 >>> 24] ^ _$cl[_$dh >> 16 & 255] ^ _$ba[_$c1 >> 8 & 255] ^ _$gi[_$iC & 255] ^ _$cx[_$ij + 3];
+        _$ij += 4;
+        _$dh = _$dR;
+        _$c1 = _$gz;
+        _$iC = _$ji;
+    }
+    for (_$je = 0; _$je < 4; _$je++) {
+        _$iR[_$df ? 3 & -_$je : _$je] = _$hp[_$dh >>> 24] << 24 ^ _$hp[_$c1 >> 16 & 255] << 16 ^ _$hp[_$iC >> 8 & 255] << 8 ^ _$hp[_$a7 & 255] ^ _$cx[_$ij++];
+        _$dR = _$dh;
+        _$dh = _$c1;
+        _$c1 = _$iC;
+        _$iC = _$a7;
+        _$a7 = _$dR;
+    }
+    return _$iR;
+}
+
+function _$i2(_$c1, _$dh) {
+    if (typeof _$c1 === 'string')
+        _$c1 = _$k4(_$c1);
+    if (!_$dh)
+        _$dh = _$cD;
+    var _$a7 = '', _$iC;
+    for (_$iC = 0; _$iC < _$c1.length; _$iC = _$iC + 3) {
+        _$a7 += _$dh[_$c1[_$iC] >> 2];
+        _$a7 += _$dh[((_$c1[_$iC] & 3) << 4) | (_$c1[_$iC + 1] >> 4)];
+        if (_$c1[_$iC + 1] !== _$lz) {
+            _$a7 += _$dh[((_$c1[_$iC + 1] & 15) << 2) | (_$c1[_$iC + 2] >> 6)];
+        }
+        if (_$c1[_$iC + 2] !== _$lz) {
+            _$a7 += _$dh[_$c1[_$iC + 2] & 63];
+        }
+    }
+    return _$a7;
+}
+
+function _$iZ() {
+    var _$iC = [];
+    for (var _$dh = 0; _$dh < 256; ++_$dh) {
+        var _$a7 = _$dh;
+        for (var _$c1 = 0; _$c1 < 8; ++_$c1) {
+            if ((_$a7 & 0x80) !== 0)
+                _$a7 = (_$a7 << 1) ^ 7;
+            else
+                _$a7 <<= 1;
+        }
+        _$iC[_$dh] = _$a7 & 0xff;
+    }
+    return _$iC;
+}
+/*
+@QQ_VX:   240942649
+@Author:  小伟科技工作室
+*/
